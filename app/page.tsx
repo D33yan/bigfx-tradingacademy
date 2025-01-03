b7eec0b0d2c5b4f03687826d72c6cac8cec599dd
D33yan <nnajidivine2006@gmail.com>
2025-01-03 07:54:28 +0100

    assets added 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "@/components/layout/sections/hero";
 import { AboutSection } from "@/components/layout/sections/about";
+import { FooterSection } from "@/components/layout/sections/footer";
 export default function Home() {
   return (
     <div>
       <HeroSection/>
       <AboutSection/>
+      <FooterSection/>
     </div>
   );
 }
